ace14cb454d4a958bbbc7f59a3640cc5c5dfebfd
Sergey Rakeev <rakeev.s@yandex.ru>
2022-05-12 22:48:28 +0300

    Add base bot logic

---
+++ data/config.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN=str(os.getenv('BOT_TOKEN'))
+
+admins=[
+    644812536
+]
+
+ip=os.getenv('ip')
+PGUSER=str(os.getenv('PGUSER'))
+PGPASSWD=str(os.getenv('PGPASSWD'))
+DATABASE=str(os.getenv('DATABASE'))
+
+
+POSTGRES_URI=f'postgresql://{PGUSER}:{PGPASSWD}@{ip}/{DATABASE}'
